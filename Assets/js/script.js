1327e1574a6cfaa67b92274458f3612ad68f407e
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-05-17 16:35:48 -0600

    worked with my tutor to sort out functionality and fine tune the color coding and local storage concepts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,85 +2,43 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMMM Do, YYYY"));
 
-// DOCUMENT SELECTOR VARIABLES
-var $rowNine = $("#row-9");
-var $rowTen = $("#row-10");
-var $rowEleven = $("#row-11");
-var $rowNoon = $("#row-12");
-var $rowOne = $("#row-1");
-var $rowTwo = $("#row-2");
-var $rowThree = $("#row-3");
-var $rowFour = $("#row-4");
-var $rowFive = $("#row-5");
+// TIME-BLOCK SELECTOR VARIABLE
+var $timeBlock = $("#time-block");
 
-var $hour = $(".hour");
-
-var $saveBtn = $("#saveBtn");
-
-var $row = $(".row");
-
-// HOUR VARIABLES
-var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-console.log(hours);
+// SAVE BUTTON DOC SELECTOR
+var $saveBtn = $("#save-button");
 
 // CURRENT HOUR
 var currentHour = moment().hour();
 console.log(currentHour);
 
-// COLOR CODE FUNCTION
-function colorCode() {
-  if (hours[0] === currentHour) {
-    $rowNine.addClass("present");
-  } else if (hours[1] === currentHour) {
-    $rowTen.addClass("present");
-  } else if (hours[2] === currentHour) {
-    $rowEleven.addClass("present");
-  } else if (hours[3] === currentHour) {
-    $rowNoon.addClass("present");
-  } else if (hours[4] === currentHour) {
-    $rowOne.addClass("present");
-  } else if (hours[5] === currentHour) {
-    $rowTwo.addClass("present");
-  } else if (hours[6] === currentHour) {
-    $rowThree.addClass("present");
-  } else if (hours[7] === currentHour) {
-    $rowFour.addClass("present");
-  } else if (hours[8] === currentHour) {
-    $rowFive.addClass("present");
-  } else {
+// HOUR VARIABLES AND FUNCTION
+var startHour = 9;
+var endHour = 17;
+for (let i = startHour; i <= endHour; i++) {
+  var $row = $(
+    `<div id="time-block" class="row">
+        <div class="col hour">
+          ${i}HR
+        </div>
+        <input id="user-input" type="text" class="col-6 border border-dark textarea">
+        </input>
+        <button id="save-button" class="col saveBtn">
+          SAVE
+        </button>
+      </div>`
+  );
+  if (i === currentHour) {
+    $row.addClass("present");
+  } else if (i < currentHour) {
     $row.addClass("past");
+  } else {
+    $row.addClass("future");
   }
+  $row.children("input").val(localStorage.getItem(i));
+  $row.on("click", ".saveBtn", function () {
+    var note = $(this).siblings("input").val();
+    localStorage.setItem(i, note);
+  });
+  $("body>div").append($row);
 }
-
-// COLOR CODE CALL
-colorCode();
-
-// TEXT ENTRY VARIABLES
-var $userInput = $("#user-input");
-
-// function saveUserInput(event) {
-//   event.preventDefault();
-//   var userText = $userInput;
-//   localStorage.getItem("toDo");
-//   localStorage.setItem("toDo", (userText));
-// }
-
-$saveBtn.on("click", function (event) {
-  event.preventDefault();
-  var text = document.querySelector("#user-input").value;
-  localStorage.setItem("toDo", text);
-  $userInput.append(text);
-});
-
-// // FOR LOOP INITIAL FOR CHANGING COLOR
-// for (let i = 0; i < hours.length; i++) {
-//   console.log("i is ", [i]);
-//   var hourRow = currentHour[i];
-//   if (hourRow === currentHour) {
-//     $row.addClass("present");
-//   } else if (hourRow < currentHour) {
-//     $row.addClass("past");
-//   } else {
-//     $row.addClass("future");
-//   }
-// }
